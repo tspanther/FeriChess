54d2cedd7eb9508a14cc98a280cd2a0a62abb3de
NikoU <niko.uremovic@student.um.si>
2018-10-28 00:30:37 +0200

    Implemented basic client side move reqests & serverside methods that will evaluate those requests.

---
+++ FeriChess/FeriChess/Controllers/GameController.cs
@@ -0,0 +1,24 @@
+﻿using FeriChess.Models;
+using Newtonsoft.Json.Linq;
+using System.Web.Http;
+
+namespace FeriChess.Controllers
+{
+    [RoutePrefix("api/game")]
+    public class GameController : ApiController
+    {
+        [Route("get-available-moves")]
+        [HttpPost]
+        public string GetAvailableMoves(Field field)
+        {
+            return string.Format("get-available-moves: {0} {1}", field.X, field.Y);
+        }
+
+        [Route("make-a-move")]
+        [HttpPost]
+        public string MakeAMove(Move move)
+        {
+            return string.Format("make-a-move: from: {0} {1} | to: {2} {3}", move.From.X, move.From.Y, move.To.X, move.To.Y);
+        }
+    }
+}
--- FeriChess/FeriChess/Scripts/ourScripts/board.js
+++ FeriChess/FeriChess/Scripts/ourScripts/board.js
@@ -1,12 +1,64 @@
-$(document).ready(function(){
-	var position = 1;
-	for (var row=0; row<8; row++) {
+$(document).ready(function () {
+	for (var row = 0; row < 8; row++) {
 		var col = "";
-		for (var column=0; column<8; column++) { 
-			col += "<td data-pos='"+position+"'></td>"; 
-			position++; 
+		for (var column = 0; column < 8; column++) {
+			col += "<td class=\"field\" data-pos='" + row + column + "'></td>";
 		}
 
-		$("#chessboard").append("<tr>"+col+"</tr>");
-    }
+		$("#chessboard").append("<tr>" + col + "</tr>");
+	}
+
+	var firstClick = true;
+	var prevClickedField = "";
+
+	function Move(pos1, pos2) {
+		var self = this;
+		self.From = pos1;
+		self.To = pos2;
+	}
+
+	function Field(pos) {
+		var self = this;
+		self.X = parseInt(pos[0]);
+		self.Y = parseInt(pos[1]);
+	}
+
+	$(".field").each(function () {
+		$(this).on("click", function () {
+			var _url = "api/game/";
+			var request;
+
+			if (firstClick) {
+				firstClick = false;
+				prevClickedField = $(this).attr("data-pos");
+
+				_url += "get-available-moves";
+				request = new Field($(this).attr("data-pos"));
+			}
+			else {
+				firstClick = true;
+				request = new Move(new Field(prevClickedField), new Field($(this).attr("data-pos")));
+
+				_url += "make-a-move";
+			}
+			request = JSON.stringify(request);
+
+			$.ajax({
+				type: "POST",
+				url: _url,
+				contentType: "application/json; charset=utf-8",
+				dataType: "json",
+				data: request,
+				success: function (data) {
+					alert(JSON.stringify(data));
+				},
+				failure: function (data) {
+					alert("shit");
+				},
+				error: function (data) {
+					alert("shitshit");
+				}
+			});
+		});
+	});
 });
