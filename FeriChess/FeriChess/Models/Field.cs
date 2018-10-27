5196bdf08ed936733432d0496bedcc9fb0c2657e
NikoU <niko.uremovic@student.um.si>
2018-10-28 00:27:31 +0200

    Added basic move & field implementation for exemplary client - server communication

---
+++ FeriChess/FeriChess/Models/Field.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FeriChess.Models
+{
+    public class Field
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+    }
+}
---
+++ FeriChess/FeriChess/Models/Move.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FeriChess.Models
+{
+    public class Move
+    {
+        public Field From { get; set; }
+        public Field To { get; set; }
+    }
+}
