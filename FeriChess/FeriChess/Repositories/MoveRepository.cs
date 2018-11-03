0139b1d72414a2fbc440b392d6f6ed946a71d612
NikoU <niko.uremovic@student.um.si>
2018-11-03 11:01:14 +0100

    Added basic repository implementation for Davor to continue his work on

---
+++ FeriChess/FeriChess/Repositories/MoveRepository.cs
@@ -0,0 +1,24 @@
+﻿using FeriChess.Models;
+using NPoco;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FeriChess.Repositories
+{
+    public class MoveRepository
+    {
+        IDatabase db = new Database("connection string...");
+
+        public List<Move> GetMoves()
+        {
+            /*
+             * also supports linq & lambda expressions
+             * read more here https://github.com/schotime/NPoco
+             */
+            List<Move> moves = db.Fetch<Move>("select * from movesTable");
+            return moves;
+        }
+    }
+}
