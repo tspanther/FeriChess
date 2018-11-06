b14701860ff03d5ea89078c06956e6451d646167
NikoU <niko.uremovic@student.um.si>
2018-11-06 15:34:14 +0100

    Simple test to try out DI

--- FeriChess/FeriChess.Tests/Controllers/GameControllerTest.cs
+++ FeriChess/FeriChess.Tests/Controllers/GameControllerTest.cs
@@ -2,12 +2,23 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using FeriChess;
 using FeriChess.Controllers;
+using Ninject;
 
 namespace FeriChess.Tests.Controllers
 {
     [TestClass]
     public class GameControllerTest
     {
+        private IKernel kernel = new StandardKernel(new App_Start.NinjectWebCommon.TestModule());
 
+        [TestMethod]
+        public void GetAvailableMovesResponseIsNotNull()
+        {
+            var controller = kernel.Get<GameController>();
+
+            var resp = controller.GetAvailableMoves(new Models.Field(1, 2));
+
+            Assert.IsNotNull(resp);
+        }
     }
 }
