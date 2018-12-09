e5f4d5b31ef9f0b9abdd0a56f40b7cb79257944a
NikoU <niko.uremovic@student.um.si>
2018-12-09 16:05:57 +0100

    Implemented MockBoardService for testing

---
+++ FeriChess/FeriChess/Services/MockBoardService.cs
@@ -0,0 +1,54 @@
+﻿using FeriChess.Interfaces;
+using FeriChess.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FeriChess.Services
+{
+    public class MockBoardService : IBoardService
+    {
+        public List<Move> GetAvailableMoves(Field f)
+        {
+            var ret = new List<Move>
+            {
+                new Move(new Field(1, 2), new Field(2, 2))
+            };
+            return ret;
+        }
+
+        public List<FieldUpdate> GetFieldUpdates(Move m)
+        {
+            var ret = new List<FieldUpdate>
+            {
+                new FieldUpdate(new Field(1, 2))
+            };
+            return ret;
+        }
+
+        public bool IsValid(Move m)
+        {
+            return true;
+        }
+
+        public string ListToString(List<Move> l)
+        {
+            return "mock";
+        }
+
+        public List<FieldUpdate> LoadBoardstate()
+        {
+            var ret = new List<FieldUpdate>
+            {
+                new FieldUpdate(new Field(1, 2))
+            };
+            return ret;
+        }
+
+        public void SetStartingPosition()
+        {
+            return;
+        }
+    }
+}
