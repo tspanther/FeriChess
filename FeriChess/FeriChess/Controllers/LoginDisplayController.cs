c2561440afa3411937d1ce2e0df74a7c3f724378
NikoU <niko.uremovic@student.um.si>
2018-12-09 18:58:55 +0100

    Integrated static login, statistics and learn pages with our API

--- FeriChess/FeriChess/Controllers/LoginDisplayController.cs
+++ FeriChess/FeriChess/Controllers/LoginDisplayController.cs
@@ -4,10 +4,15 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using System.Web.Mvc;
 
 namespace FeriChess.Controllers
 {
-    public class LoginController : ApiController
+    public class LoginDisplayController : Controller
     {
+        public ActionResult Index()
+        {
+            return View();
+        }
     }
 }
---
+++ FeriChess/FeriChess/Controllers/StatisticsDisplayController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FeriChess.Controllers
+{
+    public class StatisticsDisplayController : Controller
+    {
+        // GET: StatisticDisplay
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
