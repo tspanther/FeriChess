0df904287cd1b9f4ef8fdcaee981fd98c15bd8ce
NikoU <niko.uremovic@student.um.si>
2018-12-10 20:16:41 +0100

    Implemented GamestateChange class
    contains information on updated fields & game events

---
+++ FeriChess/FeriChess/Models/GamestateChange.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FeriChess.Models
+{
+    public class GamestateChange
+    {
+        public List<FieldUpdate> UpdateFields { get; set; }
+        public string GameResult { get; set; }
+    }
+}
